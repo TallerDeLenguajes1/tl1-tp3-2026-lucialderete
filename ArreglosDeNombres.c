db825dd7fd1ae4a6b17f5a3d8482541862aa5b18
Lucia Alderete <lu.4ld3r3t3@gmail.com>
2026-04-24 12:27:42 -0300

    parte 2.2

--- ArreglosDeNombres.c
+++ ArreglosDeNombres.c
@@ -4,33 +4,57 @@
 
 
 void MostrarPersonas(char *V[], int cant);
+int BuscarNombre(char *V[], int cant, char nombre[]);
+
 int main(){
     int cant=5;
+    int indice;
+    char nombre[120];
+
     char *V[cant];
     char buff[120];
+
     for (int i=0; i<cant; i++){
         printf("Ingrese su nombre: ");
         scanf(" %s", buff);
         int caracteres = strlen(buff);
-        V[i]= (char *)malloc(sizeof(char) * caracteres);
+        V[i]= (char *)malloc(sizeof(char) * caracteres + 1);
         strcpy(V[i], buff);
     }
     
     MostrarPersonas(V, cant);
+    printf("Ingrese un nombre: ");
+    scanf(" %s", nombre);
+    indice = BuscarNombre(V, cant, nombre);
+    if(indice != -1){
+        printf("Coincidencia encontrada: %d) %s \n", indice, V[indice]);
+    }else{
+        printf("no se encontraron coincidencias\n");
+    }
 
     for(int i=0; i<cant; i++){
         free(V[i]);
     }
-
 
     return 0;
 }
 
 void MostrarPersonas(char *V[], int cant){
     for (int i=0; i<cant;i++){
-        printf(" %s \n", V[i]);
+        printf("%d %s \n", i,  V[i]);
 
     }
 }
 
-
+int BuscarNombre(char *V[], int cant, char nombre[]){
+    char *resultado;
+    int indice;
+    //la funcion busca el nombre en el arreglo
+    for(int i=0; i<cant; i++){
+        resultado=strstr(V[i], nombre);
+        if(resultado){
+            return i;
+        }
+    }
+    return -1;
+}
