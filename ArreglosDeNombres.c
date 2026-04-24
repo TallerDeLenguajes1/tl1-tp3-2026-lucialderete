107f28463fafaf66be64425842131012670d220c
Lucia Alderete <lu.4ld3r3t3@gmail.com>
2026-04-24 11:04:55 -0300

    punto 2

--- ArreglosDeNombres.c
+++ ArreglosDeNombres.c
@@ -4,6 +4,8 @@
 
 
 void MostrarPersonas(char *V[], int cant);
+void BuscarNombre(char *V[], int cant);
+
 int main(){
     int cant=5;
     char *V[cant];
@@ -12,11 +14,12 @@ int main(){
         printf("Ingrese su nombre: ");
         scanf(" %s", buff);
         int caracteres = strlen(buff);
-        V[i]= (char *)malloc(sizeof(char) * caracteres);
+        V[i]= (char *)malloc(sizeof(char) * caracteres + 1);
         strcpy(V[i], buff);
     }
     
     MostrarPersonas(V, cant);
+    BuscarNombre(V, cant);
 
     for(int i=0; i<cant; i++){
         free(V[i]);
@@ -28,9 +31,20 @@ int main(){
 
 void MostrarPersonas(char *V[], int cant){
     for (int i=0; i<cant;i++){
-        printf(" %s \n", V[i]);
+        printf("%d) %s \n", i, V[i]);
 
     }
 }
 
+void BuscarNombre(char *V[], int cant){
+    int num;
+    printf("---Busca nombre por ID---");
+    printf("Ingrese un numero: ");
+    scanf("%d", &num);
 
+    if (num >= 0 && num < cant){
+        printf("El nombre ubicado en la posicion %d es %s:\n ", num, V[num]);
+    }else{
+        printf("no se encontró el valor buscado");
+    }
+}
